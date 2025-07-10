e7bb9d7728836f9d5b26711a65f678877ebee18c
Perrine96 <perrinemallard96@gmail.com>
2025-07-10 20:03:48 +0200

    panier simple session

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,14 +5,74 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Repository\ProductsRepository;
 
 final class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart')]
-    public function index(): Response
+    public function index(Request $request, ProductsRepository $productsRepository): Response
     {
+        $session = $request->getSession();
+        $cart = $session->get('cart', []);
+
+        $cartData = [];
+        $total = 0;
+
+        foreach ($cart as $id => $quantity) {
+            $product = $productsRepository->find($id);
+            if ($product) {
+                $cartData[] = [
+                    'id' => $product->getId(),
+                    'name' => $product->getName(),
+                    'price' => $product->getPrice(),
+                    'quantity' => $quantity,
+                    'total' => $product->getPrice() * $quantity,
+                ];
+                $total += $product->getPrice() * $quantity;
+            }
+        }
+
         return $this->render('cart/cart.html.twig', [
-            'controller_name' => 'CartController',
+            'cart' => $cartData,
+            'total' => $total,
         ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'cart_add')]
+    public function add($id, Request $request, ProductsRepository $productsRepository): Response
+    {
+        $product = $productsRepository->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+        // Vérifier le stock
+        if ($product->getQuantity() < 1) {
+            $this->addFlash('danger', 'Stock unavailable');
+            return $this->redirectToRoute('app_cart');
+        }
+
+        $session = $request->getSession();
+        $cart = $session->get('cart', []);
+        $cart[$id] = ($cart[$id] ?? 0) + 1;
+        $session->set('cart', $cart);
+
+        $this->addFlash('success', 'Product added to cart');
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'cart_remove')]
+    public function remove($id, Request $request): Response
+    {
+        $session = $request->getSession();
+        $cart = $session->get('cart', []);
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
+            $session->set('cart', $cart);
+            $this->addFlash('success', 'Product removed from cart');
+        }
+        return $this->redirectToRoute('app_cart');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,13 @@ use Symfony\Component\HttpFoundation\Request;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductsRepository $productsRepository): Response {
-        $products = $productsRepository->findAll();
+    public function index(ProductsRepository $productsRepository, Request $request): Response {
+        $type = $request->query->get('type');
+        if ($type) {
+            $products = $productsRepository->findBy(['type' => $type]);
+        } else {
+            $products = $productsRepository->findAll();
+        }
 
         return $this->render('home/home.html.twig', [
             'products' => $products,
